74fb0513a0a46e55d5e0136053b99c8b22778c95
huangyanan <huangyanan@zufangit.cn>
2015-12-25 11:11:45 +0800

    Initial Commit

---
+++ TestGroup/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGroup
+//
+//  Created by 丁丁 on 15/12/25.
+//  Copyright © 2015年 huangyanan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGroup/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGroup
+//
+//  Created by 丁丁 on 15/12/25.
+//  Copyright © 2015年 huangyanan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
